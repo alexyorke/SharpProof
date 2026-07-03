900841eaaae597aa25020e6c320e9d0b28c15eb8
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-03 10:48:50 -0700

    Cover alias disposal lifetime facts

--- PurelySharp.Test/UsingDisposeSoundnessStressTests.cs
+++ PurelySharp.Test/UsingDisposeSoundnessStressTests.cs
@@ -472,6 +472,101 @@ public sealed class TestClass
         }
 
         [Test]
+        public async Task ExplicitDisposeAliasThenDisposeOriginal_Diagnostic()
+        {
+            var test = @"
+using System;
+using PurelySharp.Attributes;
+
+public sealed class PureDisposable : IDisposable
+{
+    [EnforcePure]
+    public void Dispose()
+    {
+    }
+}
+
+public sealed class TestClass
+{
+    [EnforcePure]
+    public void {|PS0002:TestMethod|}()
+    {
+        var resource = new PureDisposable();
+        var alias = resource;
+        alias.Dispose();
+        resource.Dispose();
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
+        public async Task ExplicitDisposeAliasThenUseOriginal_Diagnostic()
+        {
+            var test = @"
+using System;
+using PurelySharp.Attributes;
+
+public sealed class PureDisposable : IDisposable
+{
+    [EnforcePure]
+    public void Dispose()
+    {
+    }
+
+    [EnforcePure]
+    public int Use()
+    {
+        return 1;
+    }
+}
+
+public sealed class TestClass
+{
+    [EnforcePure]
+    public int {|PS0002:TestMethod|}()
+    {
+        var resource = new PureDisposable();
+        var alias = resource;
+        alias.Dispose();
+        return resource.Use();
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
+        public async Task ExplicitDisposeAliasSatisfiesOwnedLocalDisposal_NoDiagnostic()
+        {
+            var test = @"
+using System;
+using PurelySharp.Attributes;
+
+public sealed class PureDisposable : IDisposable
+{
+    [EnforcePure]
+    public void Dispose()
+    {
+    }
+}
+
+public sealed class TestClass
+{
+    [EnforcePure]
+    public void TestMethod()
+    {
+        var resource = new PureDisposable();
+        var alias = resource;
+        alias.Dispose();
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task UseAfterUsingStatementExistingLocal_Diagnostic()
         {
             var test = @"
