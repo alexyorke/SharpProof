fbc91e4a98206712acc6031cf01a49bc9a063a6f
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-04 07:10:22 -0700

    Move string equality IR lowering into string partial

--- PurelySharp.Test/ArchitectureReductionTests.cs
+++ PurelySharp.Test/ArchitectureReductionTests.cs
@@ -866,10 +866,14 @@ namespace PurelySharp.Test
             Assert.That(coreSource, Does.Contain("TryLowerStringStaticValueMember(memberSymbol, out term)"));
             Assert.That(coreSource, Does.Not.Contain("private static bool TryLowerRegexIsMatchInvocation"));
             Assert.That(coreSource, Does.Not.Contain("private static bool TryLowerStringPredicateInvocation"));
+            Assert.That(coreSource, Does.Not.Contain("private static bool TryLowerStringEqualityCondition"));
+            Assert.That(coreSource, Does.Not.Contain("private static bool TryCreateStringEqualityCondition"));
             Assert.That(coreSource, Does.Not.Contain("private static bool TryLowerStringStaticValueMember"));
             Assert.That(coreSource, Does.Not.Contain("private static bool IsSystemStringType"));
             Assert.That(stringSource, Does.Contain("private static bool TryLowerRegexIsMatchInvocation"));
             Assert.That(stringSource, Does.Contain("private static bool TryLowerStringPredicateInvocation"));
+            Assert.That(stringSource, Does.Contain("private static bool TryLowerStringEqualityCondition"));
+            Assert.That(stringSource, Does.Contain("private static bool TryCreateStringEqualityCondition"));
             Assert.That(stringSource, Does.Contain("private static bool TryLowerStringStaticValueMember"));
             Assert.That(stringSource, Does.Contain("private static bool IsSystemStringType"));
         }
