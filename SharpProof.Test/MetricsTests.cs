2eb6013b2ccc3add263602fe9e55278edd665929
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-07 15:08:55 -0700

    Speed up metrics analyzer tests

--- SharpProof.Test/MetricsTests.cs
+++ SharpProof.Test/MetricsTests.cs
@@ -1,14 +1,19 @@
+using Microsoft.CodeAnalysis;
 using System.Threading.Tasks;
+using System.Collections.Immutable;
 using NUnit.Framework;
 using SharpProof.Analyzer;
-using VerifyCS = SharpProof.Test.CSharpAnalyzerVerifier<
-    SharpProof.Analyzer.SharpProofAnalyzer>;
+using static SharpProof.Test.AnalyzerTestHost;
 
 namespace SharpProof.Test
 {
     [TestFixture]
     public class MetricsTests
     {
+        private static readonly ImmutableArray<MetadataReference> MetricsFrameworkReferences =
+            GetMinimalFrameworkReferences()
+                .Add(MetadataReference.CreateFromFile(typeof(System.Diagnostics.Metrics.Meter).Assembly.Location));
+
         [Test]
         public async Task MeterCreateCounter_Diagnostic()
         {
@@ -25,7 +30,7 @@ public class TestClass
     }
 }";
 
-            await VerifyCS.VerifyAnalyzerAsync(test);
+            await AssertPurityDiagnosticAsync(test);
         }
 
         [Test]
@@ -44,7 +49,40 @@ public class TestClass
     }
 }";
 
-            await VerifyCS.VerifyAnalyzerAsync(test);
+            await AssertPurityDiagnosticAsync(test);
+        }
+
+        private static async Task AssertPurityDiagnosticAsync(string markedSource)
+        {
+            var (source, expectedSpanText) = StripSp0002Markup(markedSource);
+            var diagnostics = await GetDiagnosticsAsync(
+                source,
+                frameworkReferences: MetricsFrameworkReferences,
+                concurrentAnalysis: true);
+
+            Assert.That(diagnostics, Has.Length.EqualTo(1));
+            var diagnostic = SingleDiagnostic(diagnostics, SharpProofDiagnostics.PurityNotVerifiedId);
+            Assert.That(
+                source.Substring(
+                    diagnostic.Location.SourceSpan.Start,
+                    diagnostic.Location.SourceSpan.Length),
+                Is.EqualTo(expectedSpanText));
+        }
+
+        private static (string Source, string ExpectedSpanText) StripSp0002Markup(string markedSource)
+        {
+            const string prefix = "{|SP0002:";
+            const string suffix = "|}";
+            var start = markedSource.IndexOf(prefix, System.StringComparison.Ordinal);
+            Assert.That(start, Is.GreaterThanOrEqualTo(0), "Expected SP0002 markup start.");
+
+            var contentStart = start + prefix.Length;
+            var end = markedSource.IndexOf(suffix, contentStart, System.StringComparison.Ordinal);
+            Assert.That(end, Is.GreaterThanOrEqualTo(0), "Expected SP0002 markup end.");
+
+            var expectedSpanText = markedSource.Substring(contentStart, end - contentStart);
+            var source = markedSource.Remove(end, suffix.Length).Remove(start, prefix.Length);
+            return (source, expectedSpanText);
         }
     }
 }
