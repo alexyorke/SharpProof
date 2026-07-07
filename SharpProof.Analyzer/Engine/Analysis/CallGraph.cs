9a6289d773cf9bea271e2341a53f54e5fa745022
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-07 15:46:35 -0700

    Format call graph analysis helpers

--- SharpProof.Analyzer/Engine/Analysis/CallGraph.cs
+++ SharpProof.Analyzer/Engine/Analysis/CallGraph.cs
@@ -3,14 +3,14 @@ using Microsoft.CodeAnalysis;
 
 namespace SharpProof.Analyzer.Engine.Analysis
 {
-	internal sealed class CallGraph
-	{
-		public ImmutableDictionary<IMethodSymbol, ImmutableHashSet<IMethodSymbol>> Edges { get; }
+    internal sealed class CallGraph
+    {
+        public ImmutableDictionary<IMethodSymbol, ImmutableHashSet<IMethodSymbol>> Edges { get; }
 
-		public CallGraph(ImmutableDictionary<IMethodSymbol, ImmutableHashSet<IMethodSymbol>> edges)
-		{
-			Edges = edges;
-		}
-	}
+        public CallGraph(ImmutableDictionary<IMethodSymbol, ImmutableHashSet<IMethodSymbol>> edges)
+        {
+            Edges = edges;
+        }
+    }
 }
 
