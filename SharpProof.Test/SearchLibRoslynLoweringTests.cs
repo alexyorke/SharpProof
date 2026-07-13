92b7343bb9d71a35f102b65fbadb34ebf90ce25c
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-13 05:17:33 -0700

    Parameterize Roslyn lowering classifications

--- SharpProof.Test/SearchLibRoslynLoweringTests.cs
+++ SharpProof.Test/SearchLibRoslynLoweringTests.cs
@@ -7,12 +7,60 @@ namespace SharpProof.Test;
 [TestFixture]
 public class SearchLibRoslynLoweringTests
 {
-    [Test]
-    public void Lowering_ContradictoryImpureCallPath_ProvesPure()
+    [TestCase(
+        "int x",
+        "x > 0 && x < 0",
+        "true",
+        AnalyzerPurityHazardKind.ImpureCallReachability,
+        PurityProofOutcome.ProvablyPure,
+        "path_unsatisfiable",
+        TestName = "Lowering_ContradictoryImpureCallPath_ProvesPure")]
+    [TestCase(
+        "string s",
+        "s == null",
+        "s == null",
+        AnalyzerPurityHazardKind.NullDereference,
+        PurityProofOutcome.ProvablyImpure,
+        "null_dereference_reachable",
+        TestName = "Lowering_NullReceiverGuard_ProvesNullDereference")]
+    [TestCase(
+        "int divisor",
+        "divisor != 0",
+        "divisor == 0",
+        AnalyzerPurityHazardKind.DivideByZero,
+        PurityProofOutcome.ProvablyPure,
+        "divide_by_zero_unreachable",
+        TestName = "Lowering_NonZeroGuard_ProvesDivideByZeroUnreachable")]
+    [TestCase(
+        "int x",
+        "x + 1 <= 0 && x >= 0",
+        "true",
+        AnalyzerPurityHazardKind.ImpureCallReachability,
+        PurityProofOutcome.ProvablyPure,
+        "path_unsatisfiable",
+        TestName = "Lowering_AffineContradictoryImpureCallPath_ProvesPure")]
+    [TestCase(
+        "int divisor",
+        "divisor + 1 != 1",
+        "divisor == 0",
+        AnalyzerPurityHazardKind.DivideByZero,
+        PurityProofOutcome.ProvablyPure,
+        "divide_by_zero_unreachable",
+        TestName = "Lowering_AffineGuard_ProvesDivideByZeroUnreachable")]
+    public void Lowering_ClassifiesRoslynEvidence(
+        string parameters,
+        string pathCondition,
+        string conclusion,
+        int hazardKind,
+        PurityProofOutcome expectedOutcome,
+        string expectedReason)
     {
-        var context = AnalyzerTestHost.CreateConditionImplicationContext("int x", "x > 0 && x < 0", "true");
+        var context = AnalyzerTestHost.CreateConditionImplicationContext(
+            parameters,
+            pathCondition,
+            conclusion);
         var evidence = new AnalyzerPurityEvidence(
-            AnalyzerPurityHazardKind.ImpureCallReachability,
+            (AnalyzerPurityHazardKind)hazardKind,
             new[] { context.PathCondition },
             context.Conclusion);
 
@@ -26,100 +74,7 @@ public class SearchLibRoslynLoweringTests
         using var search = new PurityProofSearch();
         var result = search.Classify(query!, TimeSpan.FromSeconds(2));
 
-        Assert.That(result.Outcome, Is.EqualTo(PurityProofOutcome.ProvablyPure));
-        Assert.That(result.Reason, Is.EqualTo("path_unsatisfiable"));
-    }
-
-    [Test]
-    public void Lowering_NullReceiverGuard_ProvesNullDereference()
-    {
-        var context = AnalyzerTestHost.CreateConditionImplicationContext("string s", "s == null", "s == null");
-        var evidence = new AnalyzerPurityEvidence(
-            AnalyzerPurityHazardKind.NullDereference,
-            new[] { context.PathCondition },
-            context.Conclusion);
-
-        var lowered = AnalyzerEvidenceToSearchLibLowering.TryLower(
-            evidence,
-            context.SemanticModel,
-            CancellationToken.None,
-            out var query);
-
-        Assert.That(lowered, Is.True);
-        using var search = new PurityProofSearch();
-        var result = search.Classify(query!, TimeSpan.FromSeconds(2));
-
-        Assert.That(result.Outcome, Is.EqualTo(PurityProofOutcome.ProvablyImpure));
-        Assert.That(result.Reason, Is.EqualTo("null_dereference_reachable"));
-    }
-
-    [Test]
-    public void Lowering_NonZeroGuard_ProvesDivideByZeroUnreachable()
-    {
-        var context = AnalyzerTestHost.CreateConditionImplicationContext("int divisor", "divisor != 0", "divisor == 0");
-        var evidence = new AnalyzerPurityEvidence(
-            AnalyzerPurityHazardKind.DivideByZero,
-            new[] { context.PathCondition },
-            context.Conclusion);
-
-        var lowered = AnalyzerEvidenceToSearchLibLowering.TryLower(
-            evidence,
-            context.SemanticModel,
-            CancellationToken.None,
-            out var query);
-
-        Assert.That(lowered, Is.True);
-        using var search = new PurityProofSearch();
-        var result = search.Classify(query!, TimeSpan.FromSeconds(2));
-
-        Assert.That(result.Outcome, Is.EqualTo(PurityProofOutcome.ProvablyPure));
-        Assert.That(result.Reason, Is.EqualTo("divide_by_zero_unreachable"));
-    }
-
-    [Test]
-    public void Lowering_AffineContradictoryImpureCallPath_ProvesPure()
-    {
-        var context = AnalyzerTestHost.CreateConditionImplicationContext("int x", "x + 1 <= 0 && x >= 0", "true");
-        var evidence = new AnalyzerPurityEvidence(
-            AnalyzerPurityHazardKind.ImpureCallReachability,
-            new[] { context.PathCondition },
-            context.Conclusion);
-
-        var lowered = AnalyzerEvidenceToSearchLibLowering.TryLower(
-            evidence,
-            context.SemanticModel,
-            CancellationToken.None,
-            out var query);
-
-        Assert.That(lowered, Is.True);
-        using var search = new PurityProofSearch();
-        var result = search.Classify(query!, TimeSpan.FromSeconds(2));
-
-        Assert.That(result.Outcome, Is.EqualTo(PurityProofOutcome.ProvablyPure));
-        Assert.That(result.Reason, Is.EqualTo("path_unsatisfiable"));
-    }
-
-    [Test]
-    public void Lowering_AffineGuard_ProvesDivideByZeroUnreachable()
-    {
-        var context =
-            AnalyzerTestHost.CreateConditionImplicationContext("int divisor", "divisor + 1 != 1", "divisor == 0");
-        var evidence = new AnalyzerPurityEvidence(
-            AnalyzerPurityHazardKind.DivideByZero,
-            new[] { context.PathCondition },
-            context.Conclusion);
-
-        var lowered = AnalyzerEvidenceToSearchLibLowering.TryLower(
-            evidence,
-            context.SemanticModel,
-            CancellationToken.None,
-            out var query);
-
-        Assert.That(lowered, Is.True);
-        using var search = new PurityProofSearch();
-        var result = search.Classify(query!, TimeSpan.FromSeconds(2));
-
-        Assert.That(result.Outcome, Is.EqualTo(PurityProofOutcome.ProvablyPure));
-        Assert.That(result.Reason, Is.EqualTo("divide_by_zero_unreachable"));
+        Assert.That(result.Outcome, Is.EqualTo(expectedOutcome));
+        Assert.That(result.Reason, Is.EqualTo(expectedReason));
     }
 }
