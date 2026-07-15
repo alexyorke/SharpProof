0e4d3bb5903fd438d96ff68cb91d57ae87541082
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-15 06:32:31 -0700

    Use canonical completion for throwing finally

--- SharpProof.Analyzer/ExceptionPathStateService.Finally.cs
+++ SharpProof.Analyzer/ExceptionPathStateService.Finally.cs
@@ -1,10 +1,7 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using SharpProof.Analyzer.Engine;
 using SharpProof.Symbolic;
-using SharpProof.Symbolic.Ir;
 using SharpProof.Symbolic.Smt;
-using static SharpProof.Analyzer.ExceptionFlowAnalyzer;
 
 namespace SharpProof.Analyzer;
 
@@ -45,98 +42,12 @@ internal static partial class ExceptionPathStateService
     {
         var pathState = CollectPathStateForUse(site, semanticModel, cancellationToken);
         if (!IsPathStateReachable(pathState, smtAnalysis)) return false;
-        return BlockExitIsProven(finallyBlock, pathState, semanticModel, cancellationToken, smtAnalysis);
-    }
-
-    private static bool StatementExitIsProven(
-        StatementSyntax statement,
-        SymbolicState pathState,
-        SemanticModel semanticModel,
-        CancellationToken cancellationToken,
-        SmtAnalysisService smtAnalysis)
-    {
-        if (SymbolicControlFlowFacts.StatementDefinitelyExits(statement, semanticModel, cancellationToken)) return true;
-
-        switch (statement)
-        {
-            case BlockSyntax block:
-                return BlockExitIsProven(block, pathState, semanticModel, cancellationToken, smtAnalysis);
-            case IfStatementSyntax ifStatement:
-                return IfStatementExitIsProven(ifStatement, pathState, semanticModel, cancellationToken,
-                    smtAnalysis);
-            default:
-                return false;
-        }
-    }
-
-    private static bool BlockExitIsProven(
-        BlockSyntax block,
-        SymbolicState pathState,
-        SemanticModel semanticModel,
-        CancellationToken cancellationToken,
-        SmtAnalysisService smtAnalysis)
-    {
-        foreach (var statement in block.Statements)
-        {
-            var statementState = SymbolicReachabilityService.CollectPathStateAt(
-                statement,
-                semanticModel,
-                cancellationToken,
-                pathState);
-            if (StatementExitIsProven(statement, statementState, semanticModel, cancellationToken, smtAnalysis))
-                return true;
-
-            if (!IsPathStateReachable(statementState, smtAnalysis)) return false;
-        }
-
-        return false;
-    }
-
-    private static bool IfStatementExitIsProven(
-        IfStatementSyntax ifStatement,
-        SymbolicState pathState,
-        SemanticModel semanticModel,
-        CancellationToken cancellationToken,
-        SmtAnalysisService smtAnalysis)
-    {
-        var trueBranch = SymbolicReachabilityService.ApplyBranchFacts(
-            pathState,
-            ifStatement.Condition,
-            true,
+        SymbolicStatementStateTransfer.AddCompletedBlockStateFacts(
+            ref pathState,
+            finallyBlock,
             semanticModel,
             cancellationToken);
-        if (trueBranch is not { IsExact: true, Value: { } trueState })
-            return false;
-
-        var trueReachable = IsPathStateReachable(trueState, smtAnalysis);
-        var trueExits = !trueReachable ||
-                        StatementExitIsProven(ifStatement.Statement, trueState, semanticModel, cancellationToken,
-                            smtAnalysis);
-
-        if (ifStatement.Else?.Statement is not { } elseStatement)
-            return trueReachable && trueExits &&
-                   SymbolicSemanticPipeline.LowerCondition(
-                       ifStatement.Condition,
-                       new SymbolicLoweringContext(semanticModel, cancellationToken)) is
-                   { IsExact: true, Value: { } condition } &&
-                   SymbolicReachabilityService.ClassifyStateConditionTruth(pathState, condition, smtAnalysis)
-                       .Info.Status == SymbolicProofStatus.ProvenTrue;
-
-        var falseBranch = SymbolicReachabilityService.ApplyBranchFacts(
-            pathState,
-            ifStatement.Condition,
-            false,
-            semanticModel,
-            cancellationToken);
-        if (falseBranch is not { IsExact: true, Value: { } falseState })
-            return false;
-
-        var falseReachable = IsPathStateReachable(falseState, smtAnalysis);
-        var falseExits = !falseReachable ||
-                         StatementExitIsProven(elseStatement, falseState, semanticModel, cancellationToken,
-                             smtAnalysis);
-
-        return trueExits && falseExits && (trueReachable || falseReachable);
+        return !IsPathStateReachable(pathState, smtAnalysis);
     }
 
 }
