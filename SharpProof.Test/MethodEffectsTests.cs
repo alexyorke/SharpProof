1f7887889afaa26358ee768eb100ce08e192951a
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-22 07:02:16 -0700

    Add failing positional record origin test

--- SharpProof.Test/MethodEffectsTests.cs
+++ SharpProof.Test/MethodEffectsTests.cs
@@ -2877,6 +2877,26 @@ public sealed class MethodEffectsTests {
         });
     }
     [Test]
+    public void ReturnedLambdaPreservesPositionalRecordMemberOrigin() {
+        var result = Analyze("""
+            sealed class Box { public int State; }
+            sealed record Holder(Box Value);
+            class C {
+                static System.Action Bind(Box value) {
+                    var holder = new Holder(value);
+                    return () => holder.Value.State++;
+                }
+                static void M(Box value) { Bind(value)(); }
+            }
+            """, 8);
+        Assert.Multiple(() => {
+            Assert.That(result.MethodEffects!.Purity, Is.EqualTo(SharpProofVerdict.Disproven));
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesArgumentState), Is.True);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesCapturedState), Is.False);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.Unknown), Is.False);
+        });
+    }
+    [Test]
     public void ReturnedLambdaPreservesCapturedPrimaryConstructorMemberOrigin() {
         var result = Analyze("""
             sealed class Box { public int State; }
