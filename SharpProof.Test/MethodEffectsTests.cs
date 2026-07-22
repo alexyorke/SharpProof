91cb40b0c2058d26d85816263c02488290a89edf
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-22 07:53:16 -0700

    Add failing helper field deconstruction test

--- SharpProof.Test/MethodEffectsTests.cs
+++ SharpProof.Test/MethodEffectsTests.cs
@@ -3223,6 +3223,33 @@ public sealed class MethodEffectsTests {
         });
     }
     [Test]
+    public void ReturnedLambdaPreservesConstructorHelperFieldDeconstructionMemberOrigin() {
+        var result = Analyze("""
+            sealed class Box { public int State; }
+            sealed class Source {
+                public Box Value = null!;
+            }
+            sealed class Holder {
+                public Box Value;
+                private static (Box, int) Pick(Source source) => (source.Value, 0);
+                public Holder(Source source) { (Value, _) = Pick(source); }
+            }
+            class C {
+                static System.Action Bind(Source input) {
+                    var holder = new Holder(input);
+                    return () => holder.Value.State++;
+                }
+                static void M(Source input) { Bind(input)(); }
+            }
+            """, 15);
+        Assert.Multiple(() => {
+            Assert.That(result.MethodEffects!.Purity, Is.EqualTo(SharpProofVerdict.Disproven));
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesArgumentState), Is.True);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesCapturedState), Is.False);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.Unknown), Is.False);
+        });
+    }
+    [Test]
     public void ReturnedLambdaPreservesCapturedPrimaryConstructorMemberOrigin() {
         var result = Analyze("""
             sealed class Box { public int State; }
