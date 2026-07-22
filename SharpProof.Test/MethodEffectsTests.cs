f95ae852c416499fdcc35f1fa283abcbc9fb8223
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-22 02:59:47 -0700

    Add failing constant switch expression test

--- SharpProof.Test/MethodEffectsTests.cs
+++ SharpProof.Test/MethodEffectsTests.cs
@@ -111,6 +111,20 @@ public sealed class MethodEffectsTests {
             Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesStaticState), Is.True);
         });
     }
+    [Test]
+    public void ConstantSwitchExpressionSkipsUnselectedArm() {
+        var result = Analyze("""
+            class C {
+                static int state;
+                static int Mutate() { state++; return 1; }
+                static int M() => 0 switch { 0 => 1, _ => Mutate() };
+            }
+            """, 4);
+        Assert.Multiple(() => {
+            Assert.That(result.MethodEffects!.Purity, Is.EqualTo(SharpProofVerdict.Proven));
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesStaticState), Is.False);
+        });
+    }
     [TestCase("static int M(int[] values) => values.Length;", SharpProofVerdict.Proven)]
     [TestCase("static int[] M(int x) => [1, x, 3];", SharpProofVerdict.Disproven)]
     public void ArrayIntrinsicsHaveStructuralEffects(string method, SharpProofVerdict expectedAllocationFree) {
