7c3b9f091f1f0079f4ef1425160bdc650f8a3a2c
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-22 06:44:51 -0700

    Add failing constructor branch assignment test

--- SharpProof.Test/MethodEffectsTests.cs
+++ SharpProof.Test/MethodEffectsTests.cs
@@ -2751,6 +2751,34 @@ public sealed class MethodEffectsTests {
         });
     }
     [Test]
+    public void ReturnedLambdaPreservesCapturedConstructorBranchedAssignmentOrigins() {
+        var result = Analyze("""
+            sealed class Box { public int State; }
+            sealed class Holder {
+                public Box Value;
+                public Holder(Box first, Box second, bool choose) {
+                    if (choose)
+                        Value = first;
+                    else
+                        Value = second;
+                }
+            }
+            class C {
+                static System.Action Bind(Box left, Box right, bool choose) {
+                    var holder = new Holder(left, right, choose);
+                    return () => holder.Value.State++;
+                }
+                static void M(Box left, Box right, bool choose) { Bind(left, right, choose)(); }
+            }
+            """, 16);
+        Assert.Multiple(() => {
+            Assert.That(result.MethodEffects!.Purity, Is.EqualTo(SharpProofVerdict.Disproven));
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesArgumentState), Is.True);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesCapturedState), Is.False);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.Unknown), Is.False);
+        });
+    }
+    [Test]
     public void ReturnedLambdaPreservesCapturedPrimaryConstructorMemberOrigin() {
         var result = Analyze("""
             sealed class Box { public int State; }
