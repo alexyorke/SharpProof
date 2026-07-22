8198a4a982de73b6817e081490b979b1b8779f34
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-22 05:51:27 -0700

    Add failing anonymous member origin test

--- SharpProof.Test/MethodEffectsTests.cs
+++ SharpProof.Test/MethodEffectsTests.cs
@@ -2455,6 +2455,25 @@ public sealed class MethodEffectsTests {
         });
     }
     [Test]
+    public void ReturnedLambdaPreservesCapturedAnonymousMemberOrigin() {
+        var result = Analyze("""
+            sealed class Box { public int State; }
+            class C {
+                static System.Action Bind(Box value) {
+                    var holder = new { Value = value };
+                    return () => holder.Value.State++;
+                }
+                static void M(Box value) { Bind(value)(); }
+            }
+            """, 7);
+        Assert.Multiple(() => {
+            Assert.That(result.MethodEffects!.Purity, Is.EqualTo(SharpProofVerdict.Disproven));
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesArgumentState), Is.True);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesCapturedState), Is.False);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.Unknown), Is.False);
+        });
+    }
+    [Test]
     public void PropertyResultRetainsDelegateTarget() {
         var result = Analyze("""
             class C {
