cf3fa2fc9c2cac59771a47bb6bc0d2761265a9a6
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-22 02:30:18 -0700

    Add failing ref return local alias test

--- SharpProof.Test/MethodEffectsTests.cs
+++ SharpProof.Test/MethodEffectsTests.cs
@@ -390,6 +390,23 @@ public sealed class MethodEffectsTests {
         });
     }
     [Test]
+    public void RefLocalFromRefReturnWritesArgumentState() {
+        var result = Analyze("""
+            sealed class Box { public int Value; }
+            class C {
+                static ref int GetValue(Box box) => ref box.Value;
+                static void M(Box input) {
+                    ref int alias = ref GetValue(input);
+                    alias = 1;
+                }
+            }
+            """, 4);
+        Assert.Multiple(() => {
+            Assert.That(result.MethodEffects!.Purity, Is.EqualTo(SharpProofVerdict.Disproven));
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesArgumentState), Is.True);
+        });
+    }
+    [Test]
     public void RefReturnPropertyAssignmentTracksExposedStaticState() {
         var result = Analyze("""
             sealed class Box {
