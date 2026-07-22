5566cbe23b3b276b3549487cac8939e695657efb
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-22 03:05:36 -0700

    Add failing constant boolean short circuit tests

--- SharpProof.Test/MethodEffectsTests.cs
+++ SharpProof.Test/MethodEffectsTests.cs
@@ -195,6 +195,21 @@ public sealed class MethodEffectsTests {
             Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesStaticState), Is.True);
         });
     }
+    [TestCase("true || Mutate()")]
+    [TestCase("false && Mutate()")]
+    public void ConstantBooleanShortCircuitSkipsRightOperand(string expression) {
+        var result = Analyze($$"""
+            class C {
+                static int state;
+                static bool Mutate() { state++; return true; }
+                static bool M() => {{expression}};
+            }
+            """, 4);
+        Assert.Multiple(() => {
+            Assert.That(result.MethodEffects!.Purity, Is.EqualTo(SharpProofVerdict.Proven));
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesStaticState), Is.False);
+        });
+    }
     [TestCase("static int M(int[] values) => values.Length;", SharpProofVerdict.Proven)]
     [TestCase("static int[] M(int x) => [1, x, 3];", SharpProofVerdict.Disproven)]
     public void ArrayIntrinsicsHaveStructuralEffects(string method, SharpProofVerdict expectedAllocationFree) {
