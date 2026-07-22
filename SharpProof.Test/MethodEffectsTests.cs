645cf529fadf112722f93601bb822c6162aeff4a
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-22 07:49:15 -0700

    Add failing auto-property deconstruction test

--- SharpProof.Test/MethodEffectsTests.cs
+++ SharpProof.Test/MethodEffectsTests.cs
@@ -3169,6 +3169,33 @@ public sealed class MethodEffectsTests {
         });
     }
     [Test]
+    public void ReturnedLambdaPreservesConstructorAutoPropertyDeconstructionMemberOrigin() {
+        var result = Analyze("""
+            sealed class Box { public int State; }
+            sealed class Source {
+                public Box Value { get; init; } = null!;
+                public (Box, int) Pair => (Value, 0);
+            }
+            sealed class Holder {
+                public Box Value;
+                public Holder(Source source) { (Value, _) = source.Pair; }
+            }
+            class C {
+                static System.Action Bind(Source input) {
+                    var holder = new Holder(input);
+                    return () => holder.Value.State++;
+                }
+                static void M(Source input) { Bind(input)(); }
+            }
+            """, 15);
+        Assert.Multiple(() => {
+            Assert.That(result.MethodEffects!.Purity, Is.EqualTo(SharpProofVerdict.Disproven));
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesArgumentState), Is.True);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesCapturedState), Is.False);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.Unknown), Is.False);
+        });
+    }
+    [Test]
     public void ReturnedLambdaPreservesCapturedPrimaryConstructorMemberOrigin() {
         var result = Analyze("""
             sealed class Box { public int State; }
