9c85377ca9de71e523f1a3ed78415f34c0f1a6b3
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-22 04:37:31 -0700

    Add failing property result origin test

--- SharpProof.Test/MethodEffectsTests.cs
+++ SharpProof.Test/MethodEffectsTests.cs
@@ -1376,6 +1376,28 @@ public sealed class MethodEffectsTests {
         });
     }
     [Test]
+    public void PropertyResultReceiverUsesReturnedStaticOrigin() {
+        var result = Analyze("""
+            sealed class Box {
+                public int State;
+                public void Mutate() { State++; }
+            }
+            sealed class Holder {
+                private static readonly Box global = new();
+                public Box Value => global;
+            }
+            class C {
+                static void M(Holder input) { input.Value.Mutate(); }
+            }
+            """, 10);
+        Assert.Multiple(() => {
+            Assert.That(result.MethodEffects!.Purity, Is.EqualTo(SharpProofVerdict.Disproven));
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesStaticState), Is.True);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesArgumentState), Is.False);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.Unknown), Is.False);
+        });
+    }
+    [Test]
     public void PointerIndirectionAssignmentWritesArgumentState() {
         var result = Analyze("""
             unsafe class C {
