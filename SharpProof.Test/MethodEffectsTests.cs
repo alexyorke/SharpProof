4034d0c1c30cdeb104634bca591b93dfd1c023b8
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-22 07:12:19 -0700

    Add failing transitive implicit base test

--- SharpProof.Test/MethodEffectsTests.cs
+++ SharpProof.Test/MethodEffectsTests.cs
@@ -2943,6 +2943,29 @@ public sealed class MethodEffectsTests {
         });
     }
     [Test]
+    public void ReturnedLambdaPreservesTransitiveImplicitBaseConstructorMemberOrigin() {
+        var result = Analyze("""
+            sealed class Box { public int State; }
+            class GrandBase { public Box Value = C.Shared; }
+            class Base : GrandBase { }
+            sealed class Holder : Base { public Holder() { } }
+            class C {
+                internal static readonly Box Shared = new();
+                static System.Action Bind() {
+                    var holder = new Holder();
+                    return () => holder.Value.State++;
+                }
+                static void M() { Bind()(); }
+            }
+            """, 11);
+        Assert.Multiple(() => {
+            Assert.That(result.MethodEffects!.Purity, Is.EqualTo(SharpProofVerdict.Disproven));
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesStaticState), Is.True);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesCapturedState), Is.False);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.Unknown), Is.False);
+        });
+    }
+    [Test]
     public void ReturnedLambdaPreservesCapturedPrimaryConstructorMemberOrigin() {
         var result = Analyze("""
             sealed class Box { public int State; }
