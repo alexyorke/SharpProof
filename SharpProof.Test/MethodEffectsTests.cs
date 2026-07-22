e6787d1ef81ea3c75ce50d540a15edba99f7a051
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-22 01:00:52 -0700

    Add failing repeated fresh argument call test

--- SharpProof.Test/MethodEffectsTests.cs
+++ SharpProof.Test/MethodEffectsTests.cs
@@ -412,6 +412,27 @@ public sealed class MethodEffectsTests {
         });
     }
     [Test]
+    public void RepeatedStaticHelperMutationPreservesFreshOwnership() {
+        var result = Analyze("""
+            sealed class Box { public int Value; }
+            class C {
+                static void SetValue(Box box) { box.Value = 1; }
+                static int M() {
+                    var box = new Box();
+                    SetValue(box);
+                    SetValue(box);
+                    return box.Value;
+                }
+            }
+            """, 4);
+        Assert.Multiple(() => {
+            Assert.That(result.MethodEffects!.Purity, Is.EqualTo(SharpProofVerdict.Proven),
+                string.Join(" | ", result.MethodEffects.Sites.Select(static site =>
+                    site.Symbol + ":" + site.Effect + ":" + site.Reason)));
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesCapturedState), Is.False);
+        });
+    }
+    [Test]
     public void ReassignedDelegateUsesTheCurrentTarget() {
         var result = Analyze("""
             class C {
