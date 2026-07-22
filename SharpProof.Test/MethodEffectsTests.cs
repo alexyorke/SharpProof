5766aa3cdb4446f4d4cd5d3d2d12cd45a06073e6
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-22 06:19:57 -0700

    Add failing primary constructor origin test

--- SharpProof.Test/MethodEffectsTests.cs
+++ SharpProof.Test/MethodEffectsTests.cs
@@ -2650,6 +2650,26 @@ public sealed class MethodEffectsTests {
         });
     }
     [Test]
+    public void ReturnedLambdaPreservesCapturedPrimaryConstructorMemberOrigin() {
+        var result = Analyze("""
+            sealed class Box { public int State; }
+            sealed class Holder(Box value) { public Box Value = value; }
+            class C {
+                static System.Action Bind(Box value) {
+                    var holder = new Holder(value);
+                    return () => holder.Value.State++;
+                }
+                static void M(Box value) { Bind(value)(); }
+            }
+            """, 8);
+        Assert.Multiple(() => {
+            Assert.That(result.MethodEffects!.Purity, Is.EqualTo(SharpProofVerdict.Disproven));
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesArgumentState), Is.True);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.WritesCapturedState), Is.False);
+            Assert.That(result.MethodEffects.Effects.HasFlag(SharpProofEffect.Unknown), Is.False);
+        });
+    }
+    [Test]
     public void PropertyResultRetainsDelegateTarget() {
         var result = Analyze("""
             class C {
