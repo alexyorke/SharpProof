b9e6333c6bf38f751a8d89da9eba37613655b1f9
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-23 16:40:32 -0700

    Model MathF Cbrt effects

--- SharpProof.Symbolic/CompilerMethodEffectAnalysis.cs
+++ SharpProof.Symbolic/CompilerMethodEffectAnalysis.cs
@@ -3156,6 +3156,16 @@ internal sealed class MethodEffectAnalysisSession(
             } &&
             method.ContainingType.ToDisplayString() == "System.Math" &&
             method.Parameters[0] is { RefKind: RefKind.None, Type.SpecialType: SpecialType.System_Double };
+        private static bool IsMathFCbrt(IMethodSymbol method) =>
+            method is {
+                MethodKind: MethodKind.Ordinary,
+                Name: "Cbrt",
+                IsStatic: true,
+                Parameters.Length: 1,
+                ReturnType.SpecialType: SpecialType.System_Single
+            } &&
+            method.ContainingType.ToDisplayString() == "System.MathF" &&
+            method.Parameters[0] is { RefKind: RefKind.None, Type.SpecialType: SpecialType.System_Single };
         private static bool IsBitConverterToStringArray(IMethodSymbol method) =>
             method is {
                 MethodKind: MethodKind.Ordinary,
@@ -4210,6 +4220,9 @@ internal sealed class MethodEffectAnalysisSession(
                     SharpProofEffect.None,
                 ("System.Math", MethodKind.Ordinary, "Cbrt")
                     when IsMathCbrt(method) =>
+                    SharpProofEffect.None,
+                ("System.MathF", MethodKind.Ordinary, "Cbrt")
+                    when IsMathFCbrt(method) =>
                     SharpProofEffect.None,
                 ("System.Math" or "System.MathF", _, "Min" or "Max" or "Sqrt") => SharpProofEffect.None,
                 (_, _, "Parse") when numeric => SharpProofEffect.Throws,
