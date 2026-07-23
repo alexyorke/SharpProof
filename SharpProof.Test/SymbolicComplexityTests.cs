ed6822e96257dd3820bfb5153ad31000ecd35241
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-22 20:51:04 -0700

    Add failing bypassed while step test

--- SharpProof.Test/SymbolicComplexityTests.cs
+++ SharpProof.Test/SymbolicComplexityTests.cs
@@ -88,6 +88,10 @@ public sealed class SymbolicComplexityTests {
             """public static class C { public static int Work(int n) { var i=0; while(i<n){void Step(){i++;}} return i; } }""",
             "return i;", SymbolicComplexityKind.Unknown,
             unknowns: [SymbolicComplexityUnknownReason.UnsupportedWhileLoop]);
+        yield return Case("WhileLoopWithContinueBypassingStep_IsUnknown",
+            """public static class C { public static int Work(int n, bool skip) { var i=0; while(i<n){if(skip)continue;i++;} return i; } }""",
+            "return i;", SymbolicComplexityKind.Unknown,
+            unknowns: [SymbolicComplexityUnknownReason.UnsupportedWhileLoop]);
         yield return Case("KnownSourceCallee_ComposesIntoSurroundingLoop",
             """public static class C { public static void Helper(int m){for(var j=0;j<m;j++) { }} public static void Caller(int n,int m){for(var i=0;i<n;i++){Helper(m);}} }""",
             "Helper(m);", SymbolicComplexityKind.Product, "O(n * m)", callee: "Helper");
