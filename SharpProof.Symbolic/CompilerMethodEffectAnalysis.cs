f29b65246fd0f3eeee427678174714d0d6515dfa
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-07-23 16:57:14 -0700

    Model MathF Acos effects

--- SharpProof.Symbolic/CompilerMethodEffectAnalysis.cs
+++ SharpProof.Symbolic/CompilerMethodEffectAnalysis.cs
@@ -3256,6 +3256,16 @@ internal sealed class MethodEffectAnalysisSession(
             } &&
             method.ContainingType.ToDisplayString() == "System.Math" &&
             method.Parameters[0] is { RefKind: RefKind.None, Type.SpecialType: SpecialType.System_Double };
+        private static bool IsMathFAcos(IMethodSymbol method) =>
+            method is {
+                MethodKind: MethodKind.Ordinary,
+                Name: "Acos",
+                IsStatic: true,
+                Parameters.Length: 1,
+                ReturnType.SpecialType: SpecialType.System_Single
+            } &&
+            method.ContainingType.ToDisplayString() == "System.MathF" &&
+            method.Parameters[0] is { RefKind: RefKind.None, Type.SpecialType: SpecialType.System_Single };
         private static bool IsBitConverterToStringArray(IMethodSymbol method) =>
             method is {
                 MethodKind: MethodKind.Ordinary,
@@ -4340,6 +4350,9 @@ internal sealed class MethodEffectAnalysisSession(
                     SharpProofEffect.None,
                 ("System.Math", MethodKind.Ordinary, "Acos")
                     when IsMathAcos(method) =>
+                    SharpProofEffect.None,
+                ("System.MathF", MethodKind.Ordinary, "Acos")
+                    when IsMathFAcos(method) =>
                     SharpProofEffect.None,
                 ("System.Math" or "System.MathF", _, "Min" or "Max" or "Sqrt") => SharpProofEffect.None,
                 (_, _, "Parse") when numeric => SharpProofEffect.Throws,
