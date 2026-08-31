39c0cb8b69c926e698b92230fe8e6ccf085250db
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-08-30 20:51:08 -0700

    Add red ref-alias stale-fact regression

--- SharpProof.Effects.Test/IndirectLocalMutationNullnessRegressionTests.cs
+++ SharpProof.Effects.Test/IndirectLocalMutationNullnessRegressionTests.cs
@@ -3,6 +3,50 @@ namespace SharpProof.Effects.Test;
 [TestFixture]
 public sealed class IndirectLocalMutationNullnessRegressionTests
 {
+    [TestCase("WriteThroughAlias")]
+    [TestCase("ReadThroughAlias")]
+    public void RefAliasesDoNotPreserveStalePointeeFacts(
+        string methodName)
+    {
+        var compilation = EffectTestHost.CreateCompilation(
+            """
+            public static class Sample {
+                public static int WriteThroughAlias() {
+                    var divisor = 0;
+                    ref var alias = ref divisor;
+                    divisor = 1;
+                    alias = 0;
+                    return 1 / divisor;
+                }
+
+                public static int ReadThroughAlias() {
+                    var divisor = 1;
+                    ref readonly var alias = ref divisor;
+                    divisor = 0;
+                    return 1 / alias;
+                }
+            }
+            """);
+        var result = new EffectAnalysisSession(compilation).Analyze(
+            EffectTestHost.RequireMethod(
+                compilation,
+                "Sample",
+                methodName));
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(
+                result.Summary.Throws.Types.Select(static type =>
+                    type.ToDisplayString()),
+                Does.Contain("System.DivideByZeroException"),
+                methodName);
+            Assert.That(
+                result.Summary.Completeness,
+                Is.EqualTo(EffectCompleteness.Complete),
+                methodName);
+        }
+    }
+
     [TestCase("ThroughRefAlias")]
     [TestCase("ThroughLocalFunction")]
     public void IndirectMutationDoesNotSuppressReceiverEffects(
