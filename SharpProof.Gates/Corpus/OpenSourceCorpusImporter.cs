60d7d78bbdb48169c15bcde4c2c8cb8f98124d34
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-09-02 23:27:02 -0700

    Import shared hash encoding namespace

--- SharpProof.Gates/Corpus/OpenSourceCorpusImporter.cs
+++ SharpProof.Gates/Corpus/OpenSourceCorpusImporter.cs
@@ -7,6 +7,7 @@ using System.Text.Json.Serialization;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SharpProof.Ir;
 
 namespace SharpProof.Gates.Corpus;
 
